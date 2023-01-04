16c7ceb56c5823697feac772f7b61f7772440910
Pirmannnsyah <firmansyah16@mhs.mdp.ac.id>
2023-01-04 12:13:48 +0700

    konfigurasi awal

---
+++ android/app/src/main/java/com/if5a/rumors/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.if5a.rumors.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if5a.rumors.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
